a39defdea3ca1181d5d5093bb0e40d7e86c0c251
OmerKirshner <154836683+OmerKirshner@users.noreply.github.com>
2024-03-18 13:36:33 +0200

    Update HashTagTokenizer.java

--- HashTagTokenizer.java
+++ HashTagTokenizer.java
@@ -12,29 +12,37 @@ public class HashTagTokenizer {
 	public static String[] readDictionary(String fileName) {
 		String[] dictionary = new String[3000];
 
-		In in = new In(fileName);
-
-		// Your code here
+		In in = new In("dictionary.txt");
+			for (int i = 0; i < 3000; i++) {
+				dictionary[i] = in.readLine();
+			}
 
 		return dictionary;
 	}
 
 	public static boolean existInDictionary(String word, String []dictionary) {
-		// Your code here
+		for(int i = 0; i < 3000; i++){
+			if(word.equals(dictionary[i])) {
+				return true;
+			}
+		} 
+		return false;
 	}
 
 	public static void breakHashTag(String hashtag, String[] dictionary) {
 
 		// Base case: do nothing (return) if hashtag is an empty string.
-        if (hashtag.isEmpty()) {
-            return;
+        String Loweredhash = hashtag.toLowerCase();
+        if(Loweredhash.isEmpty()) return;
+        for (int i = 1; i <= Loweredhash.length(); i++) {
+        	String lowsub = Loweredhash.substring(0, i);
+        	if (existInDictionary(lowsub, dictionary)) {
+        		System.out.println(lowsub);
+        		breakHashTag(Loweredhash.substring(i), dictionary);
+        		break;
+    }
         }
- 
-        int N = hashtag.length();
-
-        for (int i = 1; i <= N; i++) {
-		
-        }
+        
     }
 
 }
