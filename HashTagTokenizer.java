a71e4e19d9dd0170401bf08411c2577b0680b318
OmerKirshner <154836683+OmerKirshner@users.noreply.github.com>
2024-03-18 16:49:45 +0200

    Update HashTagTokenizer.java

--- HashTagTokenizer.java
+++ HashTagTokenizer.java
@@ -11,38 +11,34 @@ public class HashTagTokenizer {
 
 	public static String[] readDictionary(String fileName) {
 		String[] dictionary = new String[3000];
-
 		In in = new In("dictionary.txt");
-			for (int i = 0; i < 3000; i++) {
+			for(int i = 0; i < 3000; i++){
 				dictionary[i] = in.readLine();
 			}
-
 		return dictionary;
 	}
 
-	public static boolean existInDictionary(String word, String []dictionary) {
+	public static boolean existInDictionary(String word, String[]dictionary) {
 		for(int i = 0; i < 3000; i++){
-			if(word.equals(dictionary[i])) {
+			if (word.equals(dictionary[i])) {
 				return true;
 			}
-		} 
+		}
 		return false;
 	}
 
 	public static void breakHashTag(String hashtag, String[] dictionary) {
-
+		String Loweredhash = hashtag.toLowerCase();
 		// Base case: do nothing (return) if hashtag is an empty string.
-        String Loweredhash = hashtag.toLowerCase();
-        if(Loweredhash.isEmpty()) return;
+        if (Loweredhash.isEmpty()) return;
         for (int i = 1; i <= Loweredhash.length(); i++) {
-        	String lowsub = Loweredhash.substring(0, i);
-        	if (existInDictionary(lowsub, dictionary)) {
-        		System.out.println(lowsub);
-        		breakHashTag(Loweredhash.substring(i), dictionary);
-        		break;
+			String lowsub = Loweredhash.substring(0, i);
+			if (existInDictionary(lowsub, dictionary)) {
+				System.out.println(lowsub);
+				breakHashTag(Loweredhash.substring(i), dictionary);
+				break;
     }
-        }
-        
-    }
+}
+	}
 
 }
